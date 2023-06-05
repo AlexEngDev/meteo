cf94f2ae5cf81eae906ad0d407618d17eb8e6718
Alex <alexandr1991xxx@gmail.com>
2023-06-05 17:54:54 +0200

    terzo commit

--- src/app/meteo/meteo-previsioni/meteo-previsioni.component.ts
+++ src/app/meteo/meteo-previsioni/meteo-previsioni.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { List, MeteoHourly } from 'src/app/models/meteoHourly.model';
+import { MeteoService } from 'src/app/share/meteo.service';
 
 @Component({
   selector: 'app-meteo-previsioni',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./meteo-previsioni.component.css']
 })
 export class MeteoPrevisioniComponent {
+  constructor(private http: HttpClient, private meteoService:MeteoService) { }
+
+
+  cityDataHourly?:MeteoHourly
+  refactorCityDataHourly:List[]
+  searchQuery:string = "Milan"
+
+  cityDataHourlyNoDublicates:List[];
+
+
+  searchTempHourlyByCity(){
+    this.meteoService.getCityWeatherHourlycityName(this.searchQuery).subscribe(chdata => {
+      this.cityDataHourly = chdata;
+    });
+ }
+
+ searchTempHourlyByCityRefactor(){
+  const data = this.meteoService.refactorGetCityWeatherHourlycityName(this.searchQuery).subscribe(chrdata => {
+    this.refactorCityDataHourly = chrdata;
+  });
+}
+
+
 
 }
--- src/app/share/meteo.service.ts
+++ src/app/share/meteo.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { City } from '../models/meteo.model';
 import { Observable } from 'rxjs/internal/Observable';
+import { List, MeteoHourly } from '../models/meteoHourly.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,10 @@ export class MeteoService {
   constructor(private http: HttpClient) { }
 
  //private apiBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+ //private apiUrlWeatherHourly = "api.openweathermap.org/data/2.5/forecast?q=Milan&appid=85124bf88ee3f736704131367529ca62"
  private apiBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
- private apiKey = "{apiKey}"
+ private apiUrlWeatherHourly = "https://api.openweathermap.org/data/2.5/forecast?q="
+ private apiKey = "85124bf88ee3f736704131367529ca62"
 /*
  private options = {
 	headers: new HttpHeaders({
@@ -28,7 +32,32 @@ export class MeteoService {
 	}
 
 
+getCityWeatherHourlycityName(cityName: string): Observable<MeteoHourly> {
+  let url = `${this.apiUrlWeatherHourly}${cityName}&appid=`
+  console.log(url + this.apiKey);
+
+	return this.http.get<MeteoHourly>(url + this.apiKey + `&units=metric`);
+}
 
 
+refactorGetCityWeatherHourlycityName(cityName: string): Observable<List[]> {
+  let url = `${this.apiUrlWeatherHourly}${cityName}&appid=`
 
+	return this.http.get<MeteoHourly>(url + this.apiKey + `&units=metric`).pipe(
+    map(
+      (response: MeteoHourly)=>{
+        const lista: List[] = [];
+
+        response.list.forEach(w => {
+          let one_element = new List(
+            w['dt_txt'],
+            w['main']
+          )
+          lista.push(one_element);
+        })
+        return lista;
+      }
+    )
+  )
+  }
 }
